b14fe679334634b893981d68666dddff48a8e06e
Ayomide-Israel <apraiseno152@gmail.com>
2025-02-19 23:44:22 +0100

    notable progress

--- flutterproj/lib/main.dart
+++ flutterproj/lib/main.dart
@@ -4,57 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+//MaterialApp
+//Scaffold
+//appBar
+//bottomvavigationbar stateful
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromRGBO(11, 79, 147, 1),
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: "WELCOME TO ISRAEL'S APP"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: (Image.asset("assets/images/bg.jpeg",
-          height: double.infinity, fit: BoxFit.cover)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: (Text("IN TIME")),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: "Person",
+            )
+          ],
+          onDestinationSelected: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          selectedIndex: currentIndex,
+        ),
       ),
     );
   }
