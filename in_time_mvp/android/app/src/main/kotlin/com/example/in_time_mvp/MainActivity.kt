0324217ef300e2c480f382dab4bf105de423ea43
Ayomide-Israel <apraiseno152@gmail.com>
2025-06-06 00:41:50 +0100

    first commit

---
+++ in_time_mvp/android/app/src/main/kotlin/com/example/in_time_mvp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.in_time_mvp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ in_time_mvp/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:in_time_mvp/screens/login_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(home: LoginPage()));
+}
